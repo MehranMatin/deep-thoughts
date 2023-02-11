53586f3850b184700ce92f021cbf77531fd64fa4
Mehran Matin <81208724+MehranMatin@users.noreply.github.com>
2023-02-10 22:18:21 -0500

    added mutations
    token auth

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-// import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  // This ensures that every request performs an authentication check, and the updated request object will be passed to the resolvers as the context
+  context: authMiddleware
 });
 
 const app = express();
