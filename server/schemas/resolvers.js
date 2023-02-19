0968693c1259910b5472eca2821ddf531cdcb45c
Mehran Matin <81208724+MehranMatin@users.noreply.github.com>
2023-02-18 20:17:29 -0500

    fixed bug

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,7 +7,7 @@ const resolvers = {
     // me
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({})
+        const userData = await User.findOne({ _id: context.user._id })
           .select('-__v -password')
           .populate('thoughts')
           .populate('friends');
